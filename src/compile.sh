b227e06314db4760e3c0eb0e919b62e44dbb2891
Marco Aurelio Ferrari <marcoferrari@alunos.utfpr.edu.br>
2022-01-02 08:21:56 -0300

    Fixed compile binary location

--- src/compile.sh
+++ src/compile.sh
@@ -5,7 +5,7 @@ if [[ "$1" = "D3Q19" || "$1" = "D3Q27" ]]
 then
     nvcc -gencode arch=compute_${CC},code=sm_${CC} -rdc=true --ptxas-options=-v -O3 --restrict \
         *.cu \
-        -lcudadevrt -lcurand -o ./bin/$2sim_$1_sm${CC}
+        -lcudadevrt -lcurand -o ./../bin/$2sim_$1_sm${CC}
 else
     echo "Input error, example of usage is"
     echo "sh compile.sh D3Q19 011"
