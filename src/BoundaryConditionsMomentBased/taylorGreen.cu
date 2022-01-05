a10764089aab1a2edca92889cbefc9834790ef03
Marco Aurelio Ferrari <marcoferrari@alunos.utfpr.edu.br>
2022-01-05 20:31:43 -0300

    Moment based bc. There is still some mass loss for parallel plates

--- src/BoundaryConditionsMomentBased/taylorGreen.cu
+++ src/BoundaryConditionsMomentBased/taylorGreen.cu
@@ -1,10 +1,10 @@
 #include "boundaryCondition.cuh"
 
-__device__ void gpuBoundaryConditionMom(    
-    dim3 threadIdx, dim3 blockIdx, dfloat* rhoVar, 
-    dfloat* uxVar , dfloat* uyVar , dfloat* uzVar , 
-    dfloat* pixx  , dfloat* pixy  , dfloat* pixz  , 
-    dfloat* piyy  , dfloat* piyz  , dfloat* pizz ){
+__device__ void gpuBoundaryConditionMom(
+    dim3 threadIdx, dim3 blockIdx,  dfloat* pop, dfloat& rhoVar, char dNodeType,
+    dfloat &uxVar , dfloat &uyVar , dfloat& uzVar , 
+    dfloat &pixx  , dfloat &pixy  , dfloat& pixz  , 
+    dfloat &piyy  , dfloat &piyz  , dfloat& pizz ){
     /*
     int x = threadIdx.x + blockDim.x * blockIdx.x;
     int y = threadIdx.y + blockDim.y * blockIdx.y;
@@ -107,4 +107,15 @@ __device__ void gpuBoundaryConditionMom(
     {  
     }
 */
+}
+
+__global__ void gpuInitialization_nodeType(
+    char *dNodeType)
+{
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+    int z = threadIdx.z + blockDim.z * blockIdx.z;
+    
+    dNodeType[idxNodeType(threadIdx.x, threadIdx.y, threadIdx.z,blockIdx.x, blockIdx.y, blockIdx.z)] = BULK;
+
 }
