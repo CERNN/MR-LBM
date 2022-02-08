e1b9775b4615b6220c80632ca17e24ed811295d6
Marco Aurelio Ferrari <marcoferrari@alunos.utfpr.edu.br>
2022-02-08 16:31:18 -0300

    fix boundary condition

---
+++ src/taylorGreen.cu
@@ -0,0 +1,24 @@
+#include "boundaryCondition.cuh"
+
+
+__device__ void gpuBoundaryConditionMom(
+    dfloat* pop, dfloat& rhoVar, char dNodeType,
+    dfloat &uxVar , dfloat &uyVar , dfloat& uzVar , 
+    dfloat &pixx  , dfloat &pixy  , dfloat& pixz  , 
+    dfloat &piyy  , dfloat &piyz  , dfloat& pizz ){
+
+}
+
+__global__ void gpuInitialization_nodeType(
+    char *dNodeType)
+{
+    int x = threadIdx.x + blockDim.x * blockIdx.x;
+    int y = threadIdx.y + blockDim.y * blockIdx.y;
+    int z = threadIdx.z + blockDim.z * blockIdx.z;
+    if (x >= NX || y >= NY || z >= NZ)
+        return;
+
+    
+    dNodeType[idxNodeType(threadIdx.x, threadIdx.y, threadIdx.z,blockIdx.x, blockIdx.y, blockIdx.z)] = BULK;
+
+}
